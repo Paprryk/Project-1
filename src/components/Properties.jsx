70fe812597a2156dfde79d13b7e8263f4e10fbc1
Craigm84 <craigmorris2907@gmail.com>
2023-12-08 15:01:35 +0000

    Merged

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -6,13 +6,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> d258fa7fbb20921900a412ee8a88c82e38dd51bd
 function Properties() {
   return (
     <div>
@@ -21,13 +14,6 @@ function Properties() {
       <h4>Search Property</h4>
 
     <PropertiesDisplay/>
-<<<<<<< HEAD
-=======
-
-
-
-  
->>>>>>> d258fa7fbb20921900a412ee8a88c82e38dd51bd
     </div>
   );
 }
