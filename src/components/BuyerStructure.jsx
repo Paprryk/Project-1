b67c8ab7bd77ea095e021b1eb4a0cd33ff735f35
Craigm84 <craigmorris2907@gmail.com>
2023-12-08 14:54:57 +0000

    Buyer styling

--- src/components/BuyerStructure.jsx
+++ src/components/BuyerStructure.jsx
@@ -2,13 +2,14 @@ import PropTypes from "prop-types";
 
 function BuyerStructure(props) {
     return (
+    <div>Buyer: {props.id}
         <div className="row">
-            <p className="col-2">Firstname: {props.firstname}</p>
-            <p className="col-2 ">Surname: {props.lastname}</p>
+            <p className="col-3"> {props.firstname}</p>
+            <p className="col"> {props.lastname}</p>
         </div>
+</div>
     )
 }
-
 BuyerStructure.propTypes = {
     firstname: PropTypes.string.isRequired,
     lastname: PropTypes.string.isRequired
