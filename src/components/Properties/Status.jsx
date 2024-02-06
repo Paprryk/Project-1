4f700b2df550791ff7e2053593585c8d7b2a63b7
2023temitope <topbeemak@yahoo.com>
2024-02-06 10:45:43 +0000

    added confetti code

--- src/components/Properties/Status.jsx
+++ src/components/Properties/Status.jsx
@@ -10,37 +10,43 @@ import axios from "axios";
 import React from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import BookingPage from "../Bookings/BookingPage";
-
+import confetti from "canvas-confetti";
 function Status(props) {
-const navigate = useNavigate();
-const params =useParams();
-
+    const navigate = useNavigate();
+    const params = useParams();
+    const handleConfetti = () => {
+        confetti({
+            particleCount: 500,
+            spread: 320,
+        });
+    };
     function handleClick(e, status) {
         e.preventDefault()
-
+        handleConfetti();
         axios.put("http://localhost:8085/property/update" + props.id, { status })
             .then(response => {
                 props.getProperties()
 
+
             })
-            .catch(err => console.error(err))        
-                }
+            .catch(err => console.error(err))
+    }
 
     return (
-<div>
-        <button className="btn btn-secondary" type="submit"  onClick={(e) => handleClick(e, "For Sale")}>
-        For Sale
-    </button>
-    &nbsp;
-    <button className="btn btn-warning"  type="submit" onClick={(e) => handleClick(e, "Sold")}>
-        Sold
-    </button>
-    &nbsp;
-    <button className="btn btn-danger" type="submit" onClick={(e) => handleClick(e, "Withdraw")}>
-        Withdraw
-    </button>    
-    </div>    
-    );    
+        <div>
+            <button className="btn btn-secondary" type="submit" onClick={(e) => handleClick(e, "For Sale")}>
+                For Sale
+            </button>
+            &nbsp;
+            <button className="btn btn-warning" type="submit" onClick={(e) => handleClick(e, "Sold")}>
+                Sold
+            </button>
+            &nbsp;
+            <button className="btn btn-danger" type="submit" onClick={(e) => handleClick(e, "Withdraw")}>
+                Withdraw
+            </button>
+        </div>
+    );
 }
 
 export default Status;
